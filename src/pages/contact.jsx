23d44ef406f08af6c956e07c5f372ea6e10701a4
Gautam kumar <gautamsiddharth2013@gmail.com>
2026-07-30 12:29:06 +0530

    coorect logic axios

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Navbar from "../components/Navbar";
-import Navbar from "../components/Footer";
+import Footer from "../components/Footer";
 import "../pageStyles/Contact.css"
 
 function contact() {
